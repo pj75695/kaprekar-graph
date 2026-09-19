3a196f2e25493ac3b219cc86dd3eabc099ede9b8
lordjames904 <lordjames904@gmail.com>
2026-09-19 20:44:09 +0100

    Added edge highlight

--- graph_generator.py
+++ graph_generator.py
@@ -4,16 +4,22 @@ import kaprekar
 from kaprekar import kaprekar_full
 import networkx as nx
 
+# Digit combinations without ordering to reduce noise
 orderings = ["".join(map(str, c)) for c in itertools.combinations_with_replacement(range(10), 5)]
 
 def build_and_export_graph():
     G = nx.DiGraph()
     results = kaprekar_full(orderings)
+    
     for source, path in results.items():
+        # Store complete journey on starting node
+        G.add_node(source, journey=[source] + path)
+        
         prev = source
         for target in path:
             G.add_edge(prev, target)
             prev = target
+            
     with open("graph.json", "w") as f:
         json.dump(nx.node_link_data(G), f, indent=2)
 
--- main.js
+++ main.js
@@ -11,8 +11,9 @@ data.nodes.forEach(node => {
         // Spread nodes out uniformly at random across a coordinate space
         x: Math.random() * 100,
         y: Math.random() * 100,
-        size: 6,
+        size: 3,
         color: "#3b82f6", // Vibrant blue
+        journey: node.journey || [],
     });
 });
 
@@ -21,7 +22,8 @@ data.links.forEach(link => {
     if (!graph.hasEdge(link.source, link.target)) {
         graph.addDirectedEdge(link.source, link.target, {
             type: "arrow",
-            color: "#4b5563" // Slate grey edges
+            color: "#4b5563", // Slate grey edges
+            zIndex: 0
         });
     }
 });
@@ -33,6 +35,8 @@ let selectedNode = null;
 
 
 const renderer = new Sigma(graph, container, {
+    zIndex: true,
+    
     labelColor: {
         color: "#FFD700"
     },
@@ -40,8 +44,41 @@ const renderer = new Sigma(graph, container, {
     nodeReducer: (node, data) => {
         const res = { ...data };
 
-        if (node === selectedNode) {
-            res.color = "#fa1515";
+        if (selectedNode) {
+            const journey =
+                graph.getNodeAttribute(selectedNode, "journey") || [];
+
+            if (journey.includes(node)) {
+                res.color = "#fa1515";
+                res.zIndex = 5;
+            }
+        }
+
+        return res;
+    },
+
+    edgeReducer: (edge, data) => {
+        const res = { ...data };
+
+        res.zIndex = 0;
+        res.size = 1;
+
+        if (selectedNode) {
+            const journey =
+                graph.getNodeAttribute(selectedNode, "journey") || [];
+
+            const source = graph.source(edge);
+            const target = graph.target(edge);
+
+            if (
+                journey.includes(source) &&
+                journey.includes(target)
+            ) {
+                res.color = "#fa1515";
+                res.size = 6;
+
+                res.zIndex = 10;
+            }
         }
 
         return res;
